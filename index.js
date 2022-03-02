595df1f2dbccf3d442199460530178a0d4b9f111
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-03-02 11:46:42 -0600

    printing news' titles and urls

--- index.js
+++ index.js
@@ -2,14 +2,31 @@ const puppeteer = require('puppeteer');
 
 async function getBunnies() {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true, // change to false to run visualizing the web browser
+        defaultViewport: null,
     });
 
     const page = await browser.newPage();
 
-    const url = 'https://www.codex.academy/#/';
+    const url = 'https://news.ycombinator.com/';
 
     await page.goto(url);
+
+    await page.waitForSelector('.athing');
+
+    const results = await page.$$eval('.athing', rows => {
+        return rows.map(row => {
+            const properties = {};
+            const newsElement = row.querySelector('.titlelink');
+            properties.title = newsElement.innerHTML;
+            properties.url = newsElement.getAttribute('href');
+            return properties;
+        });
+    });
+
+    console.log(results);
+
+    browser.close();
 };
 
 getBunnies();
